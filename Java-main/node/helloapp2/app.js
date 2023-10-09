c2d9fc2cde8dcb72e1f42e3662a06544ccde2115
nikitasmirnov228 <114527671+nikitasmirnov228@users.noreply.github.com>
2023-10-09 15:11:09 +0300

    Add files via upload

---
+++ Java-main/node/helloapp2/app.js
@@ -0,0 +1,12 @@
+const os = require('os');
+const greeting = require('./greeting');
+const User = require('./user.js');
+
+let eugene = new User('Eugene', 32);
+eugene.sayHi();
+
+// получим имя текущего пользователя
+let userName = os.userInfo().username;
+
+console.log(`Дата запроса: ${greeting.date}`);
+console.log(greeting.getMessage(userName));
